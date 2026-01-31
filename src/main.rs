c48a1cbf626128387ccb454a5a6a1cfb8b4f9ef8
Nikunj <mnikunj262@gmail.com>
2026-01-31 11:12:15 +0530

    update: loop for guess

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,28 @@ use std::{cmp::Ordering, io};
 fn main() {
     println!("--------- Guess the number ---------");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=6);
+    let mut try_take: u32 = 0;
 
-    println!("---------- input your guess -----------");
+    loop {
+        println!("--------- Input your guess");
+        try_take = try_take + 1;
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("--------- Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect(" ------------ Failed to read line ----------");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!(" -------- You are Win --------- "),
-        _ => println!("--------- You are Loss ---------")
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("--------- You Guess is correct");
+                break;
+            }
+            _ => println!("--------- Retry"),
+        }
     }
-    
+
+    println!("--------- Taken try {try_take}");
 }
